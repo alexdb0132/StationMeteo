b377939a313b196ba9ae5be656ecccafb9689f46
kevin briand <kevinbriand4@gmail.com>
2021-06-20 13:08:56 -0400

    Add: mqtt pub sub

--- AMOC_TP3/src/main.cpp
+++ AMOC_TP3/src/main.cpp
@@ -1,73 +1,51 @@
-#include <Arduino.h>
-#include "Capteurs/CapteurTemperature.h"
-#include "Capteurs/CapteurPression.h"
-#include "Capteurs/CapteurHumidite.h"
 #include "CapteursProxy/CapteurHumiditeProxyBME280.h"
 #include "CapteursProxy/CapteurPressionProxyBME280.h"
 #include "CapteursProxy/CapteurTemperatureAirProxyBME280.h"
 #include "CapteursProxy/CapteurTemperatureEauProxyDS18B20.h"
+#include "Capteurs/CapteurHumidite.h"
+#include "Capteurs/CapteurPression.h"
+#include "Capteurs/CapteurTemperature.h"
+#include <WiFi.h>
+#include "Mqtt/PubSub.h"
+
+/* test Mqtt + temp*/
+CapteurTemperature* temp;
+const char ssid[] = "iPhone de kevin";
+const char pass[] = "12345678";
+
+WiFiClient* net;
+PubSub* mqtt;
+
+void setup() {
+  temp = new CapteurTemperature(new CapteurTemperatureAirProxyBME280(0x76),new CapteurTemperatureEauProxyDS18B20(14));
+  net = new WiFiClient();
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+    mqtt = new PubSub(net,"172.20.10.5");
+}
+
+void loop(){
+  mqtt->publish("/hello",temp->AfficherTemperatureAir().c_str());
+  delay(5000);
+}
+
+/* test capteur
 CapteurHumidite* humidite;
 CapteurPression* pression;
 CapteurTemperature* temp;
 
-const char ssid[] = "ssid";
-const char pass[] = "pass";
-
-WiFiClient net;
-MQTTClient client;
-
-unsigned long lastMillis = 0;
-
-void connect() {
-  Serial.print("checking wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.print("\nconnecting...");
-  while (!client.connect("arduino", "public", "public")) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.println("\nconnected!");
-
-  client.subscribe("/hello");
-  // client.unsubscribe("/hello");
-}
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-
-  // Note: Do not use the client in the callback to publish, subscribe or
-  // unsubscribe as it may cause deadlocks when other things arrive while
-  // sending and receiving acknowledgments. Instead, change a global variable,
-  // or push to a queue and handle it in the loop after calling `client.loop()`.
-}
-
 void setup() {
-  /*humidite = new CapteurHumidite(new CapteurHumiditeProxyBME280(0x76));
+  Serial.begin(115200);
+  humidite = new CapteurHumidite(new CapteurHumiditeProxyBME280(0x76));
   pression = new CapteurPression(new CapteurPressionProxyBME280(0x76));
   temp = new CapteurTemperature(new CapteurTemperatureAirProxyBME280(0x76),new CapteurTemperatureEauProxyDS18B20(14));
 }
 
 void loop() {
-  client.loop();
-
-  if (!client.connected()) {
-    connect();
-  }
-
-  // publish a message roughly every second.
-  if (millis() - lastMillis > 1000) {
-    lastMillis = millis();
-    client.publish("/hello", "world");
-  }
-
-  /*Serial.println("humidite :" + humidite->AfficherHumidite());
+  Serial.println("humidite :" + humidite->AfficherHumidite());
   Serial.println("pression :" + pression->AfficherPressionAir());
   Serial.println("temperature :" + temp->AfficherTemperatureAir());
   Serial.println("temperature Eau:" + temp->AfficherTemperatureEau());
   delay(1000);
 }
+*/
